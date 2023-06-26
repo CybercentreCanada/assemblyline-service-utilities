6fad0baaefa0216597fc2ddb25dfaacaf752fd36
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2023-06-26 11:53:52 -0400

    Allow domains to be followed by digits without truncating them.

--- test/balbuzard/test_patterns.py
+++ test/balbuzard/test_patterns.py
@@ -5,6 +5,13 @@ import pytest
 from assemblyline_service_utilities.common.balbuzard.patterns import PatternMatch
 
 
+@pytest.mark.parametrize('data,domain', [
+    (b'config.edge.skype.com0', b'config.edge.skype.com')
+])
+def test_PAT_DOMAIN(data, domain):
+    assert re.search(PatternMatch.PAT_DOMAIN, data).group() == domain
+
+
 @pytest.mark.parametrize('url', [
     b'https://www.google.com.account.login:.@example.com',
     b'https://@example.com',
