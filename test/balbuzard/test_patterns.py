596a525ea998aaebcb3dab3026e31ae72031c43b
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2023-09-06 14:34:46 -0400

    Adding example URIs that match our scheme restrictions from wikipedia

--- test/balbuzard/test_patterns.py
+++ test/balbuzard/test_patterns.py
@@ -14,7 +14,12 @@ def test_PAT_IP(data, ip):
         assert match.group() == ip
 
 
-@pytest.mark.parametrize("data,domain", [(b"config.edge.skype.com0", b"config.edge.skype.com")])
+@pytest.mark.parametrize(
+    "data,domain",
+    [
+        (b"config.edge.skype.com0", b"config.edge.skype.com"),
+    ],
+)
 def test_PAT_DOMAIN(data, domain):
     match = re.search(PatternMatch.PAT_DOMAIN, data)
     if domain is None:
@@ -24,7 +29,17 @@ def test_PAT_DOMAIN(data, domain):
 
 
 @pytest.mark.parametrize(
-    "url", [b"https://www.google.com.account.login:.@example.com", b"https://@example.com", b"https://:@example.com"]
+    "url",
+    [
+        b"https://www.google.com.account.login:.@example.com",
+        b"https://@example.com",
+        b"https://:@example.com",
+        # Example URIs from https://en.wikipedia.org/wiki/Uniform_Resource_Identifier#Example_URIs
+        b"https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top",
+        b"http://[2001:db8::7]/c=GB?objectClass?one",
+        b"ftp://192.0.2.16:80/",
+        b"http://editing.com/resource/file.php?command=checkout",
+    ],
 )
 def test_PAT_URL_basic_auth(url):
     assert re.match(PatternMatch.PAT_URL, url).span() == (0, len(url))
