0d0bca51af9c04c4e6e6e9bbf4aa6ce21a69425d
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2023-09-06 10:42:18 -0400

    Add test for ip in version number

--- test/balbuzard/test_patterns.py
+++ test/balbuzard/test_patterns.py
@@ -5,17 +5,26 @@ import pytest
 from assemblyline_service_utilities.common.balbuzard.patterns import PatternMatch
 
 
-@pytest.mark.parametrize('data,domain', [
-    (b'config.edge.skype.com0', b'config.edge.skype.com')
-])
+@pytest.mark.parametrize("data,ip", [(b"12.2.1.3.0", None)])
+def test_PAT_IP(data, ip):
+    match = re.search(PatternMatch.PAT_IP, data)
+    if ip is None:
+        assert match is None
+    else:
+        assert match.group() == ip
+
+
+@pytest.mark.parametrize("data,domain", [(b"config.edge.skype.com0", b"config.edge.skype.com")])
 def test_PAT_DOMAIN(data, domain):
-    assert re.search(PatternMatch.PAT_DOMAIN, data).group() == domain
+    match = re.search(PatternMatch.PAT_DOMAIN, data)
+    if domain is None:
+        assert match is None
+    else:
+        assert match.group() == domain
 
 
-@pytest.mark.parametrize('url', [
-    b'https://www.google.com.account.login:.@example.com',
-    b'https://@example.com',
-    b'https://:@example.com'
-])
+@pytest.mark.parametrize(
+    "url", [b"https://www.google.com.account.login:.@example.com", b"https://@example.com", b"https://:@example.com"]
+)
 def test_PAT_URL_basic_auth(url):
     assert re.match(PatternMatch.PAT_URL, url).span() == (0, len(url))
