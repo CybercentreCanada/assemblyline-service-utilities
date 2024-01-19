41547e7b9d8412cc793bb8d29c2cd167ab054269
cccs-rs <ryan.samaroo@cyber.gc.ca>
2024-01-19 20:02:24 +0000

    Make sure requirements is in sync with setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+# read the contents of your requirements file
+with open(os.path.join(this_directory, "requirements.txt"), encoding="utf-8") as f:
+    requirements = f.read().split()
+
 setup(
     name="assemblyline-service-utilities",
     version=package_version,
@@ -39,15 +43,7 @@ setup(
             "test/*",
         ]
     ),
-    install_requires=[
-        "assemblyline",
-        "assemblyline-v4-service",
-        "cart",
-        "fuzzywuzzy",
-        "pefile",
-        "pillow",
-        "regex",
-    ],
+    install_requires=requirements,
     extra_requires={},
     package_data={
         "": [
