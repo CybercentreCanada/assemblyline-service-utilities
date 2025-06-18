92a8fc292608d6eb305f97b3eb2b1db9bb2284c9
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2025-06-18 21:07:53 +0000

    Monkeypatch the correct module
    Because api imports DEVELOPMENT_MODE into it's own namespace,
    patching utils only works if it hasn't been imported yet.
    Instead, import api and patch it's namespace directly.

--- assemblyline_service_utilities/testing/helper.py
+++ assemblyline_service_utilities/testing/helper.py
@@ -4,7 +4,7 @@ import re
 import shutil
 from pathlib import Path
 
-import assemblyline_v4_service.common.utils
+import assemblyline_v4_service.common.api
 import pytest
 from assemblyline.common import forge
 from assemblyline.common.dict_utils import flatten
@@ -17,7 +17,8 @@ from assemblyline_v4_service.common.task import Task
 from cart import unpack_file
 
 # Test in development mode
-assemblyline_v4_service.common.utils.DEVELOPMENT_MODE = True
+# Patching api instead of utils because at this point api is already imported with the original value
+assemblyline_v4_service.common.api.DEVELOPMENT_MODE = True
 
 class FileMissing(Exception):
     pass
